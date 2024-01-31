5c0ce1cfd86d801e091ede16b9227987c360dc95
victorhcunha <victor.cunha@liferay.com>
2024-01-31 10:58:00 -0300

    fix space

--- object_exercise2/src/entities/Employee.java
+++ object_exercise2/src/entities/Employee.java
@@ -39,6 +39,6 @@ public class Employee {
 
 	@java.lang.Override
 	public java.lang.String toString() {
-		return name + ", $" +  String.format("%.2f", getNetSalary());
+		return name + ", $ " +  String.format("%.2f", getNetSalary());
 	}
 }
