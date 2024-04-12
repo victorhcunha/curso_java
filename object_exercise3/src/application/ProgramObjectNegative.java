33fd82fe838420cdc015820a0f84c9309ae7abb8
victorhcunha <victor.cunha@liferay.com>
2024-04-12 13:31:06 -0300

    renamed files

--- object_exercise3/src/application/ProgramObjectNegative.java
+++ object_exercise3/src/application/ProgramObjectNegative.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 import entities.Student;
 
-public class Program {
+public class ProgramObjectNegative {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
