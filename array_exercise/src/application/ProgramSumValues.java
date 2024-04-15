618d00ab2eeac81963e2eb4e9498303988694d81
victorhcunha <victor.cunha@liferay.com>
2024-04-15 12:33:48 -0300

    fix print values

--- array_exercise/src/application/ProgramSumValues.java
+++ array_exercise/src/application/ProgramSumValues.java
@@ -5,31 +5,33 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ProgramSumValues {
-	public static void main(String[] args) {
-		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.print("Quantos números voce vai digitar? ");
-		int number = sc.nextInt();
-		number = Math.abs(number);
+    public static void main(String[] args) {
 
-		double sum = 0;
-		double average = 0;
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
-		double[] vector = new double[Math.abs(number)];
+        System.out.print("Quantos números voce vai digitar? ");
+        int number = sc.nextInt();
+        number = Math.abs(number);
 
-		for (int i = 0; i < number; i++) {
-			System.out.print("Digite um número: ");
-			vector[i] = sc.nextDouble();
-			sum += vector[i];
-			average += vector[i]/number;
-		}
+        StringBuilder values = new StringBuilder();
+        double sum = 0;
+        double average = 0;
 
-		System.out.printf("%nVALORES: %s %n", Arrays.toString(vector));
-		System.out.printf("SOMA: %.2f %n", sum);
-		System.out.printf("MÉDIA: %.2f", average);
+        double[] vector = new double[Math.abs(number)];
 
-		sc.close();
-	}
+        for (int i = 0; i < number; i++) {
+            System.out.print("Digite um número: ");
+            vector[i] = sc.nextDouble();
+            sum += vector[i];
+            average += vector[i] / number;
+            values.append(vector[i]).append(" ");
+        }
+
+        System.out.printf("%nVALORES: %s %n", values.toString());
+        System.out.printf("SOMA: %.2f %n", sum);
+        System.out.printf("MÉDIA: %.2f", average);
+
+        sc.close();
+    }
 }
