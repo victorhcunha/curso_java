9a8e6bc0e81d4541c88daf5a4627f0a4946c52aa
victorhcunha <victor.cunha@liferay.com>
2024-04-23 09:08:44 -0300

    restore ProgramOlder

--- vector_exercise/src/application/ProgramOlder.java
+++ vector_exercise/src/application/ProgramOlder.java
@@ -1,4 +1,38 @@
 package vector_exercise.src.application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProgramOlder {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Quantas pessoas voce vai digitar? ");
+        int number = sc.nextInt();
+
+        String[] names = new String[number];
+        int[] ages = new int[number];
+
+        int olderAge = 0;
+        String olderName = "";
+
+        for (int i = 0; i < number; i++) {
+            System.out.printf("Dados da %sa pessoa: %n", i + 1);
+            System.out.print("Nome: ");
+            names[i] = sc.next();
+            System.out.print("Idade: ");
+            ages[i] = sc.nextInt();
+
+            if (ages[i] > olderAge) {
+                olderAge = ages[i];
+                olderName = names[i];
+            }
+        }
+
+        System.out.printf("PESSOA MAIS VELHA: %s", olderName);
+
+        sc.close();
+
+    }
 }
